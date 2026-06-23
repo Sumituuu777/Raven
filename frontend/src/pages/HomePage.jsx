31fbc6c7f819ea41b48118526a40a490cd4f2d62
Sumit kumar <sumit.ydv.kumar2006@gmail.com>
2026-06-23 22:23:26 +0530

    ui fix for responsiveness in home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,15 +1,27 @@
-import React, { useState } from 'react'
+import React, { useContext } from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
+import { ChatContext } from '../../context/ChatContext'
 
 const HomePage = () => {
-  const [selectedUser,setSelectedUser]=useState(false)
+
+  const { selectedUser } = useContext(ChatContext)
+
   return (
     <div className='border w-full h-screen sm:px-[10%] sm:py-[3%]'>
-      <div className="backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full   grid grid-cols-1 relative md:grid-cols-[2fr_3fr]">
-        
-        <Sidebar/>
-        <ChatContainer/>
+      <div className='backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full relative md:grid md:grid-cols-[2fr_3fr]'>
+
+        {/* Mobile View */}
+        <div className='md:hidden h-full'>
+          {selectedUser ? <ChatContainer /> : <Sidebar />}
+        </div>
+
+        {/* Desktop View */}
+        <div className='hidden md:contents'>
+          <Sidebar />
+          <ChatContainer />
+        </div>
+
       </div>
     </div>
   )
