3391ee180c0003dcd446a6556be96fc5c951ddbf
Sumit kumar <sumit.ydv.kumar2006@gmail.com>
2026-06-23 23:05:27 +0530

    ui fix mobile screen 2

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -8,24 +8,24 @@ const HomePage = () => {
   const { selectedUser } = useContext(ChatContext)
 
   return (
-    <div className='h-screen p-2 sm:px-[10%] sm:py-[3%] overflow-hidden'>
+    <div className='h-screen flex items-center justify-center p-2 sm:px-[10%] sm:py-[3%] overflow-hidden'>
 
-      <div className='backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full md:grid md:grid-cols-[2fr_3fr]'>
+  <div className='backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[85dvh] w-full md:h-full md:grid md:grid-cols-[2fr_3fr]'>
 
-        {/* Mobile */}
-        <div className='md:hidden h-full'>
-          {selectedUser ? <ChatContainer /> : <Sidebar />}
-        </div>
-
-        {/* Desktop */}
-        <div className='hidden md:contents'>
-          <Sidebar />
-          <ChatContainer />
-        </div>
-
-      </div>
-
+    {/* Mobile */}
+    <div className='md:hidden h-full'>
+      {selectedUser ? <ChatContainer /> : <Sidebar />}
     </div>
+
+    {/* Desktop */}
+    <div className='hidden md:contents'>
+      <Sidebar />
+      <ChatContainer />
+    </div>
+
+  </div>
+
+</div>
   )
 }
 
