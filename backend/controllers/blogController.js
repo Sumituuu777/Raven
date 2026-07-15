7ebb8565512d3d2aa3918cde18b9be4ca7afcc7f
Sumit kumar <sumit.ydv.kumar2006@gmail.com>
2026-07-16 00:52:29 +0530

    implemented update blogs , get all blogs, and corrected create blog functions

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,42 +1,61 @@
 import { extractTags } from "../lib/utils";
 import Blogs from "../Models/blog.js"
+import cloudinary from "../lib/cloudinary.js"
+import { io, userSocketMap } from "../app.js";
 
 
 export const CreateBlog = async (req, res) => {
     try {
         const { title, content, coverImage } = req.body;
         const author = req.user._id;
+        if (!title || !content) {
+            return res.json({
+                success: false,
+                message: "Title and content are required."
+            });
+        }
         const tags = extractTags(content);
 
-        let newblog;
+        let newBlog;
         if (!coverImage) {
 
             newBlog = await Blogs.create({ title, content, tags, author })
-            res.json({ success: true, blog: newBlog })
+
         } else {
-            console.log("Profile pic starts with:", profilePic.substring(0, 50));
+
             const upload = await cloudinary.uploader.upload(coverImage, {
                 folder: "blog_covers",
                 resource_type: "image",
             });
 
             newBlog = await Blogs.create({ title, content, tags, author, coverImage: { url: upload.secure_url, public_id: upload.public_id } })
-            res.json({ success: true, blog: newBlog })
+
         }
+        for (const userId in userSocketMap) {
+                if (userId !== author.toString()) {
+                    io.to(userSocketMap[userId]).emit("newBlogCreated");
+                }
+        }
+        return res.json({ success: true, blog: newBlog })
 
     } catch (error) {
         console.log(error.message)
-        res.json({ success: false, message: error.message })
+        return res.json({ success: false, message: error.message })
     }
 }
-//-----------------------------get update blog-------------------------------------------
+//-----------------------------get all blog-------------------------------------------
 
 export const getallBlogs = async (req, res) => {
     try {
+        const AllBlogs = await Blogs.find()
+            .populate("author", "name profilePic")
+            .sort({ createdAt: -1 });
+
+        return res.json({ success: true, blogs: AllBlogs })
 
     } catch (error) {
         console.log(error.message)
-        res.json({ success: false, message: error.message })
+        return res.json({ success: false, message: error.message })
     }
 }
 //----------------------------- post update blog-------------------------------------------
@@ -44,9 +63,60 @@ export const getallBlogs = async (req, res) => {
 export const updateBlog = async (req, res) => {
     try {
 
+        const oldBlog = await Blogs.findById(blogId)
+            if (!oldBlog) {
+                return res.json({
+                    success: false,
+                    message: "original blog not found"
+                })
+            }
+
+            // only authorized person should delete his blog
+            if (oldBlog.author.toString() !== req.user._id.toString()) {
+                return res.json({
+                    success: false,
+                    message: "Unauthorized"
+                });
+            }
+
+        const { title, content, coverImage } = req.body;
+        const blogId = req.params.blogId;
+        if (!title || !content) {
+            return res.json({
+                success: false,
+                message: "Title and content are required."
+            });
+        }
+        const tags = extractTags(content);
+
+        let updatedBlog;
+        if (!coverImage) {
+
+            updatedBlog = await Blogs.findByIdAndUpdate(blogId, { title, content, tags }, { returnDocument: "after" })
+            return res.json({ success: true, blog: updatedBlog })
+
+        } else {
+            
+            const upload = await cloudinary.uploader.upload(coverImage, {
+                folder: "blog_covers",
+                resource_type: "image",
+            });
+            
+            updatedBlog = await Blogs.findByIdAndUpdate(blogId, { title, content, tags, coverImage: { url: upload.secure_url, public_id: upload.public_id } }, { returnDocument: "after" })
+
+            // after successfully uploading new image, deleting the old image, good thing to do.
+            // deleting only if image existed, checked by ? operator.
+
+            if (oldBlog.coverImage?.public_id) {
+                await cloudinary.uploader.destroy(oldBlog.coverImage.public_id);
+            }
+
+            return res.json({ success: true, blog: updatedBlog })
+        }
+
     } catch (error) {
         console.log(error.message)
-        res.json({ success: false, message: error.message })
+        return res.json({ success: false, message: error.message })
     }
 }
 //-----------------------------delete blog-------------------------------------------
